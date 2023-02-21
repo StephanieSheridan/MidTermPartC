672114ef248cf102759e06c4dd4bff9dd117eb96
StephanieSheridan <122400761+StephanieSheridan@users.noreply.github.com>
2023-02-22 03:17:24 +0800

    Add files via upload

---
+++ MidtermExam_VersionA_ExtensionCode/src/midtermexam_versiona_extensioncode/TestUserFile.java
@@ -0,0 +1,28 @@
+package midtermexam_versiona_extensioncode;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Stephanie
+ */
+import java.util.Scanner;
+public class TestUserFile {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+                
+    String userID;//the userID
+    String genre;// the user's preferred genre of movie
+    String[] genres;
+    while (true){
+    System.out.println("Please type your username");
+    userID = sc.next();
+    
+    System.out.println("Please type your favorite genre from \"Comedy\", \"Drama\", \"Action\", \"Mystery\"");
+    genre = sc.next();
+    }    
+    }
+}
